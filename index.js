1ed38cf0fde97a26d5fe72e1ce1d4f2ce82b0fd9
durai <duraiessakimuthu@gmail.com>
2024-08-16 17:48:49 +0530

    update

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const cors = require('cors');
 const api = require('./api');
 
 const app = express();
-app.use(cors());
+
+var whitelist = ['https://apssite.vercel.app/']
+var corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
+
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
