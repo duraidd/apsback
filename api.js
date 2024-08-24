131078e0417749109846dfdbefdbfa17a07cdc86
durai <duraiessakimuthu@gmail.com>
2024-08-24 10:17:59 +0530

    update

--- api.js
+++ api.js
@@ -9,46 +9,63 @@ dotenv.config();
 router.post('/send', (req, res) => {
   const { name, email, subject, message } = req.body;
 
+  let emailId = ['hr@apstechnologies.co.in','admin@apstechnologies.co.in']
+
+  let c = 0;
+
+  emailId.forEach((data)=>{
+
+    c=c+1;
+
+    var transporter = nodemailer.createTransport({
+      host: 'smtp.hostinger.com',
+      port: 587,
+      secure: false, // or 'STARTTLS'
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS
+      }
+    });
   
+    var mailOptions = {
+      from: 'contact@apstechnologies.co.in',
+      to: data,
+      subject: subject,
+      html: `
+        <html lang="en">    
+          <body>
+            <h2>
+              Name : ${name}            
+            </h2>
+            <h3>Email: ${email}</h3>
+            <p>
+              ${message}
+            </p>
+            <img src="https://apssite.vercel.app/static/media/Logo.5a71d3cd837e49a76adc.jpg" height="100px" width="200px" />
+          </body>
+        </html>
+      `
+    };
+  
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
 
-  var transporter = nodemailer.createTransport({
-    host: 'smtp.hostinger.com',
-    port: 587,
-    secure: false, // or 'STARTTLS'
-    auth: {
-      user: process.env.USER,
-      pass: process.env.PASS
-    }
-  });
+        if(c=== emailId.length){
+          res.json({ status: 200, message: "Your Details has Received" });
+          console.log('Email sent: ' + info.response);
+        }
 
-  var mailOptions = {
-    from: 'contact@apstechnologies.co.in',
-    to: 'hr@apstechnologies.co.in',
-    subject: subject,
-    html: `
-      <html lang="en">    
-        <body>
-          <h2>
-            Name : ${name}            
-          </h2>
-          <h3>Email: ${email}</h3>
-          <p>
-            ${message}
-          </p>
-          <img src="https://apssite.vercel.app/static/media/Logo.5a71d3cd837e49a76adc.jpg" height="100px" width="200px" />
-        </body>
-      </html>
-    `
-  };
+       
+      }
+    });
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      res.json({ status: 200, message: "Your Details has Received" });
-      console.log('Email sent: ' + info.response);
-    }
-  });
+
+  })
+
+
+ 
 
 
 
